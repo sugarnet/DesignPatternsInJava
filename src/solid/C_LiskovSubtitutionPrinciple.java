1e1b7dc460a53183c6a9e191e7db8e6c44c3b232
Diego Scifo <diegoscifo@gmail.com>
2020-04-16 10:08:01 -0300

    Add LSP and ISP. Change names.

---
+++ src/solid/C_LiskovSubtitutionPrinciple.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solid;
+
+/**
+ *
+ * @author Almis
+ */
+public class C_LiskovSubtitutionPrinciple {
+    
+    static void useIt(Rectangle r) {
+        int width = r.getWidth();
+        r.setHeight(10);
+        // area = width * 10;
+        System.out.println("Expected area of " + (width * 10) +
+                ", got " + r.getArea());
+    }
+    
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(2, 3);
+        useIt(r);
+        
+        Rectangle s = new Square(2);
+        useIt(s);
+    }
+    
+}
+
+class Rectangle {
+    protected int width;
+    protected int height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getArea() {
+        return width * height;
+    }
+    
+    public boolean isSquare() {
+        return width == height;
+    }
+    
+    @Override
+    public String toString() {
+        return "Rectangle{" + "width=" + width + ", height=" + height + '}';
+    }
+    
+}
+
+class RectangleFactory {
+    public static Rectangle newRectangle(int width, int height) {
+        return new Rectangle(width, height);
+    }
+    
+    public static Rectangle newSquare(int side) {
+        return new Rectangle(side, side);
+    }
+}
+
+class Square extends Rectangle {
+
+    public Square() {
+    }
+    
+    public Square(int size) {
+        width = height = size;
+    }
+
+    @Override
+    public void setHeight(int height) {
+        super.setHeight(height); //To change body of generated methods, choose Tools | Templates.
+        super.setWidth(height); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setWidth(int width) {
+        super.setWidth(width); //To change body of generated methods, choose Tools | Templates.
+        super.setHeight(width); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
---
+++ src/solid/D_InterfaceSegregationPrinciple.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solid;
+
+/**
+ *
+ * @author Almis
+ */
+public class D_InterfaceSegregationPrinciple {
+    
+}
+
+class Document {
+    
+}
+
+interface Machine {
+    void print(Document d);
+    void fax(Document d);
+    void scan(Document d);
+}
+
+class MultiFunctionPrinter implements Machine {
+
+    @Override
+    public void print(Document d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void fax(Document d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void scan(Document d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
+
+class OldFashionedPrinter implements Machine {
+
+    @Override
+    public void print(Document d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void fax(Document d) {
+        // ???
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void scan(Document d) {
+        // ???
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
+
+interface Printer {
+    void print(Document d);
+}
+
+interface Scanner {
+    void scan(Document d);
+}
+
+class JustAPrint implements Printer {
+
+    @Override
+    public void print(Document d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
+
+interface MultiFunctionDevice extends Printer, Scanner {
+    
+}
+
+class MultiFuctionMachine implements MultiFunctionDevice {
+    
+    private final Printer printer;
+    private final Scanner scanner;
+
+    public MultiFuctionMachine(Printer printer, Scanner scanner) {
+        this.printer = printer;
+        this.scanner = scanner;
+    }
+    
+    @Override
+    public void print(Document d) {
+        printer.print(d);
+    }
+
+    @Override
+    public void scan(Document d) {
+        scanner.scan(d);
+    }
+    
+}
